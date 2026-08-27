be2d3240ac3676cf9bc20794440c5e6523ce12bf
lotus <lotus@spechshop.com>
2026-08-27 16:16:57 -0400

    Adiciona implementação da classe `gsmChannel` com suporte para codificação e decodificação GSM

--- bcg729.c
+++ bcg729.c
@@ -12,6 +12,14 @@
 #include "bcg729/encoder.h"
 
 #define Z_BCG729_CHANNEL_P(zv)  ((bcg729Channel *)((char *)(Z_OBJ_P(zv)) - XtOffsetOf(bcg729Channel, std)))
+#define Z_GSM_CHANNEL_P(zv)     ((gsmChannel *)((char *)(Z_OBJ_P(zv)) - XtOffsetOf(gsmChannel, std)))
+
+#define GSM_SAMPLE_RATE          8000U
+#define GSM_SAMPLES_PER_FRAME    160U
+#define GSM_PCM_BYTES_PER_FRAME  320U
+#define GSM_BYTES_PER_FRAME      33U
+#define GSM_FRAME_DURATION_MS    20U
+#define GSM_MAX_FRAMES_PER_CALL  50U
 
 typedef struct {
     bcg729DecoderChannelContextStruct *decoder;
@@ -19,8 +27,16 @@ typedef struct {
     zend_object std;
 } bcg729Channel;
 
+typedef struct {
+    gsm encoder;
+    gsm decoder;
+    zend_object std;
+} gsmChannel;
+
 static zend_class_entry *bcg729_ce;
 static zend_object_handlers bcg729_handlers;
+static zend_class_entry *gsm_ce;
+static zend_object_handlers gsm_handlers;
 
 static zend_object *bcg729_create(zend_class_entry *ce) {
     bcg729Channel *obj = zend_object_alloc(sizeof(bcg729Channel), ce);
@@ -44,6 +60,37 @@ static void bcg729_free(zend_object *object) {
         closeBcg729EncoderChannel(obj->encoder);
         obj->encoder = NULL;
     }
+    zend_object_std_dtor(&obj->std);
+}
+
+static void gsm_channel_close(gsmChannel *obj) {
+    if (obj->encoder) {
+        gsm_destroy(obj->encoder);
+        obj->encoder = NULL;
+    }
+    if (obj->decoder) {
+        gsm_destroy(obj->decoder);
+        obj->decoder = NULL;
+    }
+}
+
+static zend_object *gsm_create_object(zend_class_entry *ce) {
+    gsmChannel *obj = zend_object_alloc(sizeof(gsmChannel), ce);
+
+    obj->encoder = gsm_create();
+    obj->decoder = gsm_create();
+
+    zend_object_std_init(&obj->std, ce);
+    object_properties_init(&obj->std, ce);
+    obj->std.handlers = &gsm_handlers;
+
+    return &obj->std;
+}
+
+static void gsm_free_object(zend_object *object) {
+    gsmChannel *obj = (gsmChannel *) ((char *) object - XtOffsetOf(gsmChannel, std));
+
+    gsm_channel_close(obj);
     zend_object_std_dtor(&obj->std);
 }
 
@@ -705,6 +752,113 @@ ZEND_METHOD(bcg729Channel, close) {
 }
 
 /* ------------------------------------------------------------------------- */
+/*    Classe gsmChannel                                                       */
+/* ------------------------------------------------------------------------- */
+
+ZEND_METHOD(gsmChannel, __construct) {}
+
+ZEND_METHOD(gsmChannel, encode) {
+    zend_string *input;
+
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+        Z_PARAM_STR(input)
+    ZEND_PARSE_PARAMETERS_END();
+
+    const size_t len = ZSTR_LEN(input);
+    if (len == 0 || (len % GSM_PCM_BYTES_PER_FRAME) != 0) {
+        RETURN_FALSE;
+    }
+
+    const size_t frames = len / GSM_PCM_BYTES_PER_FRAME;
+    if (frames > GSM_MAX_FRAMES_PER_CALL || frames > (SIZE_MAX / GSM_BYTES_PER_FRAME)) {
+        RETURN_FALSE;
+    }
+
+    gsmChannel *self = Z_GSM_CHANNEL_P(getThis());
+    if (!self->encoder) {
+        RETURN_FALSE;
+    }
+
+    const size_t out_len = frames * GSM_BYTES_PER_FRAME;
+    zend_string *out = zend_string_alloc(out_len, 0);
+    gsm_signal *src = (gsm_signal *) ZSTR_VAL(input);
+    gsm_byte *dst = (gsm_byte *) ZSTR_VAL(out);
+
+    for (size_t i = 0; i < frames; i++) {
+        gsm_encode(
+            self->encoder,
+            src + (i * GSM_SAMPLES_PER_FRAME),
+            dst + (i * GSM_BYTES_PER_FRAME)
+        );
+    }
+
+    ZSTR_VAL(out)[out_len] = '\0';
+    RETURN_STR(out);
+}
+
+ZEND_METHOD(gsmChannel, decode) {
+    zend_string *input;
+
+    ZEND_PARSE_PARAMETERS_START(1, 1)
+        Z_PARAM_STR(input)
+    ZEND_PARSE_PARAMETERS_END();
+
+    const size_t len = ZSTR_LEN(input);
+    if (len == 0 || (len % GSM_BYTES_PER_FRAME) != 0) {
+        RETURN_FALSE;
+    }
+
+    const size_t frames = len / GSM_BYTES_PER_FRAME;
+    if (frames > GSM_MAX_FRAMES_PER_CALL || frames > (SIZE_MAX / GSM_PCM_BYTES_PER_FRAME)) {
+        RETURN_FALSE;
+    }
+
+    gsmChannel *self = Z_GSM_CHANNEL_P(getThis());
+    if (!self->decoder) {
+        RETURN_FALSE;
+    }
+
+    const size_t out_len = frames * GSM_PCM_BYTES_PER_FRAME;
+    zend_string *out = zend_string_alloc(out_len, 0);
+    gsm_byte *src = (gsm_byte *) ZSTR_VAL(input);
+    gsm_signal *dst = (gsm_signal *) ZSTR_VAL(out);
+
+    for (size_t i = 0; i < frames; i++) {
+        if (gsm_decode(
+                self->decoder,
+                src + (i * GSM_BYTES_PER_FRAME),
+                dst + (i * GSM_SAMPLES_PER_FRAME)
+            ) != 0) {
+            zend_string_release(out);
+            RETURN_FALSE;
+        }
+    }
+
+    ZSTR_VAL(out)[out_len] = '\0';
+    RETURN_STR(out);
+}
+
+ZEND_METHOD(gsmChannel, info) {
+    gsmChannel *self = Z_GSM_CHANNEL_P(getThis());
+
+    array_init(return_value);
+    add_assoc_bool(return_value, "encoder_initialized", self->encoder != NULL);
+    add_assoc_bool(return_value, "decoder_initialized", self->decoder != NULL);
+    add_assoc_long(return_value, "sample_rate", GSM_SAMPLE_RATE);
+    add_assoc_long(return_value, "samples_per_frame", GSM_SAMPLES_PER_FRAME);
+    add_assoc_long(return_value, "pcm_bytes_per_frame", GSM_PCM_BYTES_PER_FRAME);
+    add_assoc_long(return_value, "gsm_bytes_per_frame", GSM_BYTES_PER_FRAME);
+    add_assoc_long(return_value, "frame_duration_ms", GSM_FRAME_DURATION_MS);
+}
+
+ZEND_METHOD(gsmChannel, close) {
+    gsmChannel *self = Z_GSM_CHANNEL_P(getThis());
+
+    gsm_channel_close(self);
+    RETURN_TRUE;
+}
+
+/* ------------------------------------------------------------------------- */
 /*    Arginfo / function tables                                               */
 /* ------------------------------------------------------------------------- */
 
@@ -724,8 +878,30 @@ static const zend_function_entry bcg729_methods[] = {
     ZEND_FE_END
 };
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_gsm_construct, 0, 0, IS_VOID, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_gsm_codec_io, 0, 1, MAY_BE_STRING | MAY_BE_FALSE)
+    ZEND_ARG_TYPE_INFO(0, input, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_gsm_info, 0, 0, IS_ARRAY, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_gsm_close, 0, 0, _IS_BOOL, 0)
+ZEND_END_ARG_INFO()
+
+static const zend_function_entry gsm_methods[] = {
+    ZEND_ME(gsmChannel, __construct, arginfo_gsm_construct, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
+    ZEND_ME(gsmChannel, encode,      arginfo_gsm_codec_io,  ZEND_ACC_PUBLIC)
+    ZEND_ME(gsmChannel, decode,      arginfo_gsm_codec_io,  ZEND_ACC_PUBLIC)
+    ZEND_ME(gsmChannel, info,        arginfo_gsm_info,      ZEND_ACC_PUBLIC)
+    ZEND_ME(gsmChannel, close,       arginfo_gsm_close,     ZEND_ACC_PUBLIC)
+    ZEND_FE_END
+};
+
 PHP_MINIT_FUNCTION(bcg729) {
-    zend_class_entry ce, gsm_ce;
+    zend_class_entry ce, gsm_class_entry;
     INIT_CLASS_ENTRY(ce, "bcg729Channel", bcg729_methods);
     bcg729_ce = zend_register_internal_class(&ce);
     bcg729_ce->create_object = bcg729_create;
@@ -734,8 +910,13 @@ PHP_MINIT_FUNCTION(bcg729) {
     bcg729_handlers.offset = XtOffsetOf(bcg729Channel, std);
     bcg729_handlers.free_obj = bcg729_free;
 
-    INIT_CLASS_ENTRY(gsm_ce, "gsmChannel", NULL);
-    zend_register_internal_class(&gsm_ce);
+    INIT_CLASS_ENTRY(gsm_class_entry, "gsmChannel", gsm_methods);
+    gsm_ce = zend_register_internal_class(&gsm_class_entry);
+    gsm_ce->create_object = gsm_create_object;
+
+    memcpy(&gsm_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+    gsm_handlers.offset = XtOffsetOf(gsmChannel, std);
+    gsm_handlers.free_obj = gsm_free_object;
 
     return SUCCESS;
 }
